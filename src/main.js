2fe457b3653c59d55527b755b77663b2a5f3f236
dogrod <rodrickjue@hotmail.com>
2017-05-19 17:24:11 +0800

    refactor: add lodash and axios support

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import lodash from 'lodash'
+
 import App from './app'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+// clone axios to Vue.$http
+Vue.prototype.$http = axios
+Vue.prototype._ = lodash
 
 /* eslint-disable no-new */
 new Vue({
